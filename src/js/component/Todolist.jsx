5921e0f41e3267196edc793b4bbd06a2e9d1e31e
Carlos Atanes Vences <carlos117g@gmail.com>
2024-01-16 19:38:29 +0000

    retoques de estilo

--- src/js/component/Todolist.jsx
+++ src/js/component/Todolist.jsx
@@ -9,7 +9,7 @@ export const Todolist = () => {
 
     // Funciones
 
-    function handleChange(event) {setInputValue(event.target.value)};
+    function handleChange(event) { setInputValue(event.target.value) };
 
     function handleSubmit(event) {
         event.preventDefault()
@@ -25,23 +25,28 @@ export const Todolist = () => {
         setTareas(newList);
     };
 
-    function handleSelect(i) {setActiveIndex(i);};
+    function handleSelect(i) { setActiveIndex(i); };
 
     // HTML
 
     return (
         <div className="container">
-            <h1 className="text-secondary">Lista de tareas</h1>
-            <form onSubmit={handleSubmit}>
-                <input className="form-control" type='text' value={inputValue} onChange={handleChange} placeholder="Regar las plantas" />
-            </form>
-            <ul className="list-group">
-                {tareas.map((tarea, i) => (
-                    <li className="list-group-item" key={i} onMouseEnter={(index) => handleSelect(i)} onMouseLeave={() => handleSelect(null)}>{tarea}
-                        <button className={`btn btn-close ${activeIndex === i ? '' : 'visually-hidden'}`} onClick={() => handleDelete(i)} key={i}></button>
-                    </li>
-                ))}
-            </ul>
+            <h1 className="text-secondary text-center mt-5">Lista de tareas</h1>
+            <div className="row d-flex justify-content-center">
+                <form className="col-xs-auto col-md-6" onSubmit={handleSubmit}>
+                    <input className="form-control" type='text' value={inputValue} onChange={handleChange} placeholder="Regar las plantas" />
+                </form>
+            </div>
+            <div className="row d-flex justify-content-center">
+                <ul className="col-xs-auto col-md-6 list-group mt-4">
+                    {tareas.map((tarea, i) => (
+                        <li className="list-group-item d-flex justify-content-between align-items-center text-wrap text-break" key={i} onMouseEnter={(index) => handleSelect(i)} onMouseLeave={() => handleSelect(null)}>
+                            {tarea}
+                            <button className={`btn btn-close${activeIndex === i ? '' : 'visually-hidden'}`} onClick={() => handleDelete(i)} key={i}></button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 }
